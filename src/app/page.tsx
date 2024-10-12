283437db3ace4a640d11a58e737c1cc592279c0b
HamBird <122409142+HamBird@users.noreply.github.com>
2024-10-11 23:50:21 -0600

    Added Project Files

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const details = ["Home", "About Me", "Projects", "Education"]
+
+export default function Home() {
+  return (
+    <div>
+      To be added!
+    </div>
+  );
+}
+
+export function Header() {
+  return (
+    <header>
+      <div>
+        
+      </div>
+    </header>
+  )
+}
